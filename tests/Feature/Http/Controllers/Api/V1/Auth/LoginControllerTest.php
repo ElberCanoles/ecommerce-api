a429507d49a2e3755983033aacf0db4f7b541bb3
Elber Canoles <elbercanoles@gmail.com>
2023-08-07 19:17:16 -0500

    test: login controller

---
+++ tests/Feature/Http/Controllers/Api/V1/Auth/LoginControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Http\Controllers\Api\V1\Auth;
+
+use App\Domain\Users\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class LoginControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private User $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
+
+    public function test_guest_user_can_login_with_correct_credentials(): void
+    {
+        $this->postJson(route(name: 'api.v1.login'), [
+            'email' => $this->user->email,
+            'password' => 'password'
+        ])
+            ->assertOk()
+            ->assertJson(function (AssertableJson $json) {
+                $json->has(key: 'access_token');
+            });
+    }
+
+    public function test_guest_user_can_not_login_with_wrong_credentials(): void
+    {
+        $this->postJson(route(name: 'api.v1.login'), [
+            'email' => $this->user->email,
+            'password' => 'wrong-password'
+        ])
+            ->assertUnprocessable()
+            ->assertJson(function (AssertableJson $json) {
+                $json->where(key: 'message', expected: trans(key: 'auth.failed'));
+            });
+    }
+
+}
